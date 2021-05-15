cbff05df3339bd788d90b7f51e53c9d6759679f0
Olivier Otshudi <olivierots@hotmail.com>
2021-05-15 18:19:01 +0100

    tidy up repo

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# copy the new Jar to the build location
-cp -f java-app/target/*.jar jenkins/build/
-
-echo "************************** building docker image **********************************"
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
-
-
